9de461ec192763a4dbe4f4cb864d03784a7ed0fa
mahtabfarrokh <mahtab.farrokh@yahoo.com>
2019-02-06 09:04:52 +0330

    date changed

--- model.py
+++ model.py
@@ -1,29 +1,27 @@
 import pandas as pd
-import jalali
+from pandas_jalali.converter import get_gregorian_date_from_jalali_date
+
 
 import numpy as np
 import matplotlib.pyplot as plt
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 import statsmodels.api as sm
 
-new_data = pd.read_csv("new_data.csv")
-for i in range(100) :
-    new_data.loc[i,'Log_Date'] = jalali.Persian( new_data.loc[i,'Log_Date']).gregorian_string("{}/{}/{}")
-    if i%100 == 0 :
-       # print(jalali.Persian( new_data.loc[i,'Log_Date']).gregorian_string("{}/{}/{}"))
-        print( new_data.loc[i,'Log_Date'] )
-# print(new_data)
 
-new_data = new_data[0:100]
-print(new_data)
-#plt.plot(new_data["Log_Date"], new_data["COUNT"])
-#plt.show()
-new_data['Log_Date'] = pd.to_datetime(new_data['Log_Date'],format='%Y/%m/%d')
+new_data = pd.read_csv("new_data.csv")
+print(new_data.head())
+
+# plt.plot(new_data["Log_Date"], new_data["COUNT"])
+# plt.show()
+
+
+
+
+new_data['Log_Date'] = pd.to_datetime(new_data['Log_Date'], format='%Y/%m/%d')
 
 print(new_data)
 # print(new_data)
 # normilize data
-
 
 
 # model
@@ -32,8 +30,8 @@ print(new_data)
 # define model configuration
 
 # define model
-new_data = new_data.drop("FROM",axis = 1)
-new_data = new_data.drop("TO",axis = 1)
+new_data = new_data.drop("FROM", axis=1)
+new_data = new_data.drop("TO", axis=1)
 
 print(new_data)
 new_data2 = new_data.values
@@ -41,9 +39,7 @@ new_data2 = new_data.values
 # resDiff = sm.tsa.arma_order_select_ic(new_data, max_ar=5, max_ma=5, ic='aic', trend='c')
 # print('ARMA(p,q) =',resDiff['aic_min_order'],'is the best.')
 
-arima = sm.tsa.statespace.SARIMAX(new_data,order=(72,1,7),freq='D',seasonal_order=(0,0,0,0),
-                                  enforce_stationarity=False, enforce_invertibility=False,).fit
+arima = sm.tsa.statespace.SARIMAX(new_data, order=(72, 1, 7), freq='D', seasonal_order=(0, 0, 0, 0),
+                                  enforce_stationarity=False, enforce_invertibility=False, ).fit
 #
 # model_fit = model.fit()
-
-
--- preprocessing.py
+++ preprocessing.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import time
 import matplotlib.pyplot as plt
+import pandas as pd
+from pandas_jalali.converter import get_gregorian_date_from_jalali_date
 
 
 data = pd.read_csv("data.csv", index_col=0)
@@ -24,4 +26,24 @@ new_data = data.groupby(['Log_Date', 'FROM', 'TO'])['Log_Date'].count()
 # print(new_data)
 # new_data = pd.DataFrame(np.array(new_data), columns=["Log_Date", "FROM", "TO", "COUNT"])
 # print(data.head())
-new_data.to_csv("new_data.csv")
+new_data['year'] = new_data['Log_Date'].apply(lambda x: int(x.split("/")[0]))
+new_data['month'] = new_data['Log_Date'].apply(lambda x: int(x.split("/")[1]))
+new_data['day'] = new_data['Log_Date'].apply(lambda x: int(x.split("/")[2]))
+new_data['year_gregorian'], new_data['month_gregorian'], new_data['day_gregorian'] = \
+    get_gregorian_date_from_jalali_date(new_data['year'], new_data['month'], new_data['day'])
+
+new_data['year_gregorian'] = new_data['year_gregorian'].apply(lambda x: str(int(x)))
+new_data['month_gregorian'] = new_data['month_gregorian'].apply(lambda x: str(int(x)))
+new_data['day_gregorian'] = new_data['day_gregorian'].apply(lambda x: str(int(x)))
+
+new_data['Log_Date'] = new_data['year_gregorian'] + '-' + new_data['month_gregorian'] + '-' + new_data['day_gregorian']
+new_data['Log_Date'] = pd.to_datetime(new_data['Log_Date'])
+print(new_data.head())
+new_data = new_data.drop('year_gregorian', axis=1)
+new_data = new_data.drop('month_gregorian', axis=1)
+new_data = new_data.drop('day_gregorian', axis=1)
+new_data = new_data.drop('year', axis=1)
+new_data = new_data.drop('month', axis=1)
+new_data = new_data.drop('day', axis=1)
+print(new_data.head())
+new_data.to_csv("new_data.csv", index=False)
