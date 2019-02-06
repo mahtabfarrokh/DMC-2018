c937e0bae09321727f98606ab4e40bde88cac565
mahtabfarrokh <mahtab.farrokh@yahoo.com>
2019-02-06 10:58:23 +0330

    date changed

--- model.py
+++ model.py
@@ -8,18 +8,29 @@ from statsmodels.tsa.statespace.sarimax import SARIMAX
 import statsmodels.api as sm
 
 
+
+
 new_data = pd.read_csv("new_data.csv")
 print(new_data.head())
+start = new_data['Log_Date'].min()
+end = new_data['Log_Date'].max()
+r = pd.date_range(start=new_data['Log_Date'].min(), end=new_data['Log_Date'].max())
+new_data = new_data.drop("FROM", axis=1)
+new_data = new_data.drop("TO", axis=1)
+new_data = new_data.groupby(['Log_Date']).count().reset_index()
+# new_data.reset_index()
+print(new_data.head())
+print("==========")
+new_data = pd.DataFrame(np.array(new_data), columns=['Log_Date', 'Count'])
+print(new_data.head())
+print("==========")
+new_data.set_index('Log_Date').reindex(r).fillna(0.0).rename_axis('Log_Date').reset_index()
+print(new_data.head())
 
-# plt.plot(new_data["Log_Date"], new_data["COUNT"])
-# plt.show()
+print("==================================")
 
+# new_data['Log_Date'] = pd.to_datetime(new_data['Log_Date'], format='%Y/%m/%d')
 
-
-
-new_data['Log_Date'] = pd.to_datetime(new_data['Log_Date'], format='%Y/%m/%d')
-
-print(new_data)
 # print(new_data)
 # normilize data
 
@@ -30,16 +41,24 @@ print(new_data)
 # define model configuration
 
 # define model
-new_data = new_data.drop("FROM", axis=1)
-new_data = new_data.drop("TO", axis=1)
+# new_data = new_data.drop("FROM", axis=1)
+# new_data = new_data.drop("TO", axis=1)
 
-print(new_data)
-new_data2 = new_data.values
+# print(new_data)
+# new_data = new_data.values
 # new_data2 = np.array(new_data)
 # resDiff = sm.tsa.arma_order_select_ic(new_data, max_ar=5, max_ma=5, ic='aic', trend='c')
 # print('ARMA(p,q) =',resDiff['aic_min_order'],'is the best.')
-
-arima = sm.tsa.statespace.SARIMAX(new_data, order=(72, 1, 7), freq='D', seasonal_order=(0, 0, 0, 0),
-                                  enforce_stationarity=False, enforce_invertibility=False, ).fit
+# print(np.asarray(new_data))
+# new_data2 = new_data['Count'][start:end].dropna()
+# new_data['year'] = new_data['Log_Date'].apply(lambda x: int(x.split("/")[0]))
+# print(new_data)
+# new_data = new_data.reset_index()
+print('---------------->', new_data)
+new_data.reset_index(drop=True, inplace=True)
+ffff = pd.DataFrame(np.array(new_data), columns=['a', 'b'], index=False)
+print('-------')
+print(ffff)
+arima = sm.tsa.statespace.SARIMAX(np.asarray(new_data), order=(72, 1, 7), seasonal_order=(0, 0, 0, 0))
 #
 # model_fit = model.fit()
