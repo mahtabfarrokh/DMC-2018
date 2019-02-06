978365a5a4f13faf330aec3318ad51a7086b4c32
mahtabfarrokh <mahtab.farrokh@yahoo.com>
2019-02-06 22:14:28 +0330

    date changed

--- preprocessing2.py
+++ preprocessing2.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+import copy
 
 # data = pd.read_csv("new_data.csv")
 # print(data.head())
@@ -11,8 +11,31 @@ data = pd.read_csv("new_data.csv")
 print(new_data.head())
 print("=======================")
 counter = 0
+res = []
 for i in range(len(new_data['FROM'])):
+    res.append([new_data['FROM'][i], new_data['TO'][i], counter])
     print(new_data['FROM'][i], new_data['TO'][i])
     s = data.loc[(data['FROM'] == new_data['FROM'][i]) & (data['TO'] == new_data['TO'][i])]
     s.to_csv("dataset/data" + str(counter)+".csv", index=False)
     counter += 1
+
+pd.DataFrame(res, columns=['FROM','TO', 'FILE']).to_csv("to-from.csv", index=False)
+for i in range(counter):
+    s =pd.read_csv("dataset/data" + str(i)+".csv")
+    s2 = copy.copy(s)
+    s = s.drop("FROM", axis=1)
+    s = s.drop("TO", axis=1)
+
+    idx = pd.date_range(start=s2['Log_Date'].min(), end=s2['Log_Date'].max())
+    s = pd.Series(s['COUNT'].values, index=s['Log_Date'])
+    print(s.head())
+    s.index = pd.DatetimeIndex(s.index)
+    s = s.reindex(idx, fill_value=0)
+    print(s.head())
+    # f = pd.DataFrame(np.array(s), columns=['Log_Date', 'COUNT'])
+    # from_1 = [s2['FROM'][i] for i in range(len)]
+    # to_1 = [s2['TO'][i] for i in range(len(s.value))]
+    s = pd.DataFrame({'Log_Date': s.index, 'COUNT': s.values})
+    print(s.head())
+    s.to_csv("dataset/dataa" + str(i) + ".csv", index=False)
+    # for j
--- srima_model.py
+++ srima_model.py
@@ -4,15 +4,18 @@ from pandas import DataFrame
 from statsmodels.tsa.arima_model import ARIMA
 from matplotlib import pyplot
 import statsmodels.api as sm
+import numpy as np
 
 number_of_dataset = 366
-new_data2 = pd.read_csv("dataset/data" + str(i) + ".csv")
-for i in range(number_of_dataset):
+result_list = []
+to_from = pd.read_csv('to-from.csv')
+final_test = pd.read_csv('final_test.csv')
 
-    print("-----------------------------------------------------------------",i)
-    new_data = pd.read_csv("dataset/data"+str(i)+".csv",header=0, parse_dates=[0], index_col=0, squeeze=True)
-    new_data2 = pd.read_csv("dataset/data" + str(i) + ".csv")
+for i in range(10):
 
+    print("-----------------------------------------------------------------", i)
+    new_data = pd.read_csv("dataset/dataa"+str(i)+".csv",header=0, parse_dates=[0], index_col=0, squeeze=True)
+    new_data2 = pd.read_csv("dataset/dataa" + str(i) + ".csv")
     if len(new_data) > 10:
         # print(new_data)
         start = new_data2["Log_Date"].min()
@@ -22,23 +25,54 @@ for i in range(number_of_dataset):
         # r = pd.date_range(start="4/13/2017", end="3/15/2018")
         # new_data.set_index('Log_Date').reindex(r).fillna(0.0).rename_axis('Log_Date').reset_index()
         test = read_csv('final_test.csv')
-        new_data = new_data.drop("FROM", axis=1)
-        new_data = new_data.drop("TO", axis=1)
+        # new_data = new_data.drop("FROM", axis=1)
+        # new_data = new_data.drop("TO", axis=1)
 
         print(new_data)
         mod = sm.tsa.statespace.SARIMAX(new_data, trend='n', order=(1,1,1), seasonal_order=(0,0,0,12),enforce_stationarity = False , enforce_invertibility = False)
         results = mod.fit()
-
+        result_list.append(results)
         # print (results.summary())
-        forecast = results.predict(start = "4/13/2018" , end = "3/15/2019")
+        # forecast = results.predict(start="2018-03-21", end="2018-03-21")
+        # forecast_list.append(forecast)
         # final_predict = []
         # for index, row in test.iterrows():
         #     final_predict.append(forecast[row["Log_Date"]])
         #
 
-        print(forecast)
+        # print(forecast)
         # test["Sales"] = final_predict
 
         # test.to_csv("new_test.csv", index=False)
 
         # print(test)
+    else:
+        result_list.append(0)
+
+c = 0
+fiiiiinaaaal = []
+for i in range(10):
+
+    for j in range(c, len(to_from['TO'])):
+        if to_from['TO'][j] == final_test['To'][i] and to_from['FROM'][j] == final_test['From'][i]:
+            c = j
+            index_found = to_from['FILE'][j]
+            if result_list[j] == 0:
+                fiiiiinaaaal.append(0)
+                print("=======================================================")
+                print(0)
+            else:
+                forecast = result_list[j].predict(start=final_test['Log_Date'][i], end=final_test['Log_Date'][i+10])
+                print("=======================================================")
+                print(forecast)
+                fiiiiinaaaal.append(forecast)
+            break
+
+x =pd.DataFrame(np.array(fiiiiinaaaal), columns=['finalres'])
+x.to_csv('final.csv', index=False)
+
+
+
+
+
+
